cf684fc853fc1aec410b753dfca9c483c86e0aa0
Rushikesh Patil <patilrushikesh8683@gmail.com>
2023-04-15 16:52:45 +0530

    By Using Redux Create Counter App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { upValue, downValue, clearValue } from './Redux/Action';
+
 
 function App() {
+  const { value } = useSelector((select) => select.myReducer)
+  // console.log(value);
+
+  const dispatch = useDispatch()
+
+  const decrementValue = () => {
+
+    dispatch(downValue(value))
+  }
+  const incrementValue = () => {
+    dispatch(upValue(value))
+  }
+  const resetValue = () => {
+    dispatch(clearValue(value))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h1>Counter APP using <span style={{fontSize:'5rem',color:value%2===0 ? '#AD7BE9':'#EA8FEA'}}>R</span>edux</h1>
+        <div className='Counter' style={{background:value%2===0 ? '#AD7BE9':'#EA8FEA'}}>
+          <button onClick={decrementValue}>Decrement -1</button> <span>{value}</span> <button onClick={incrementValue}>Increment +1</button>
+        </div>
+        <button onClick={resetValue} className='btn'>Reset</button>
+      </div>
+    </>
   );
 }
 
